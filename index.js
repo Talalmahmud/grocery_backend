c0f98e4cfcdb8270e8fdb14f0f715c079cf89b17
Ghuddy <mahmudtalal2@gmail.com>
2023-10-02 11:17:37 +0600

    add vercel config

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const rootRoute = require("./routes/product.route");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// Import the Sequelize instance
 app.use("/api/v1", rootRoute);
 app.use((req, res) => {
   res.status(404).send("Route does not exist.....");
