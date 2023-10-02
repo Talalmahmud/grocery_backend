caa340abcafa3e43ea3e2a9c5e7dc0cc3bda4aee
Ghuddy <mahmudtalal2@gmail.com>
2023-10-02 23:11:58 +0600

    ...

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express");
-const app = express();
+
 const cors = require("cors");
+const app = express();
+app.use(cors());
 require("dotenv").config();
 const rootRoute = require("./routes/product.route");
+const { readdirSync } = require("fs");
+
+readdirSync("./routes").map((file) =>
+  app.use("/api/v1", require("./routes/" + file))
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
-app.use("/api/v1", rootRoute);
+// app.use("/api/v1", rootRoute);
 
 app.use("/", (req, res) => {
   res.status(200).json({ msg: "check" });
