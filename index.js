b4891b2101b3dff69c37ef6a3c7458f59c7c1b7a
Ghuddy <mahmudtalal2@gmail.com>
2023-10-02 23:48:32 +0600

    ..

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
-app.use("/api/v1", rootRoute);
+app.use("api/v1", rootRoute);
 
 app.use("/", (req, res) => {
   res.status(200).json({ msg: "check" });
