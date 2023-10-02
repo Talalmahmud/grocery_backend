b0631eaa7967d516e7ee006953c370b7df076ad5
Ghuddy <mahmudtalal2@gmail.com>
2023-10-02 18:58:15 +0600

    ...

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", rootRoute);
-app.use((req, res) => {
+app.use(async (req, res) => {
   res.status(404).send("Route does not exist.....");
 });
 app.use(async (err, req, res, next) => {
