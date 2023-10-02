ea1e85a264815c3f9acd882869eeca0dd7cbb86d
Ghuddy <mahmudtalal2@gmail.com>
2023-10-02 19:06:07 +0600

    ...

--- index.js
+++ index.js
@@ -9,11 +9,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", rootRoute);
-app.use(async (req, res) => {
-  res.status(404).send("Route does not exist.....");
-});
-app.use(async (err, req, res, next) => {
-  return res.status(500).json({
+
+app.get("*", (err, req, res, next) => {
+  return res.status(404).json({
     msg: "Some thing is wrong , please try again",
   });
 });
@@ -21,9 +19,6 @@ app.use(async (err, req, res, next) => {
 const port = process.env.PORT;
 const start = () => {
   app.listen(port, () => {
-    if (sequelize) {
-      console.log("db connect");
-    }
     console.log(`server is running: http://localhost:${port}`);
   });
 };
